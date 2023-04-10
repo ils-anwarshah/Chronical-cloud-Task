366e17248a1369f3c4d365379960a68ffbaedbcd
Anwar Shah <a.shah@thesynapses.com>
2023-04-10 17:17:25 +0530

    Chart JS added, JS added with profile dropdown,sidebar Drawer added

--- dashboard.js
+++ dashboard.js
@@ -16,5 +16,35 @@ toggleStepRoster()
 let dropdownArrow = document.getElementById('dropdown-arrow');
 
 dropdownArrow.addEventListener("click",(e)=>{
-    console.log("GG")
-})
+    let profileDropdown = document.getElementById('profile-dropdown');
+    if(profileDropdown.style.display === "block"){
+        profileDropdown.style.display = "none";
+    }
+    else{
+        profileDropdown.style.display = "block"
+    }
+    })
+let DrawerIcon = document.getElementById('drawer-icon');
+
+function OpenNav(){
+    let profileDropdown = document.getElementById('profile-dropdown');
+    let SidebarContainer = document.getElementById('sidebar-container')
+    SidebarContainer.style.left = "0px";
+    profileDropdown.style.display = "none";
+}
+    
+function closeNav(){
+    let SidebarContainer = document.getElementById('sidebar-container');
+    let profileDropdown = document.getElementById('profile-dropdown');
+    SidebarContainer.style.left = "-300px"
+    profileDropdown.style.display = "none";
+
+}
+    
+function hideAll() {
+
+     closeNav();
+     let profileDropdown = document.getElementById('profile-dropdown');
+     profileDropdown.style.display = "none";
+    
+}
